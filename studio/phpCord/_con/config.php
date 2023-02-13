358b8fd2313f3fde5f639ed63bb74f8eff44b429
kilogrammie <117412844+kilogrammie@users.noreply.github.com>
2023-02-13 14:42:03 -0800

    Add files via upload

---
+++ studio/phpCord/_con/config.php
@@ -0,0 +1,23 @@
+﻿<?php
+// do not edit this if you dont understand what is all about 
+
+// strict requirement don not touch this !
+declare(strict_types=1); 
+
+
+
+ // change you host name from localhost to yours and database name to yours
+ $dsn="mysql:host=localhost;dbname=kilogram_vani196";
+ // you can set user here
+ $username="kilogram_vani196";
+ // you can set password here
+ $password="-!Ss]3-ey!pI71Rx";
+ 
+ 
+ $conn = new PDO( $dsn, $username, $password );
+  try {  $conn = new PDO( $dsn, $username, $password );  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION ); } catch ( PDOException $e ) {  echo "Connection failed: " . $e->getMessage(); }
+ 
+ 
+
+
+?>
